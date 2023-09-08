c541dc9685d9afebba893bf8e3e265d62d786d0b
Jassi Bacha <klikster@gmail.com>
2023-09-07 17:26:34 -0700

    Update invoices PUT method to allow changing paid status

--- routes/invoices.js
+++ routes/invoices.js
@@ -60,21 +60,41 @@ router.post('/', async (req, res, next) => {
     }
 });
 
-/** PATCH /id - Updates an invoice. 404 if empty. JSON {amt} - returns `{{invoice: {id, comp_code, amt, paid, add_date, paid_date}}` */
-router.patch('/:id', async (req, res, next) => {
+/** PUT /id - Updates an invoice. 404 if empty. JSON {amt} - returns `{{invoice: {id, comp_code, amt, paid, add_date, paid_date}}` */
+router.put('/:id', async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const { amt } = req.body;
-        const results = await db.query(
-            'UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING id, comp_code, amt, paid, add_date, paid_date',
-            [amt, id]
+        let { id } = req.params;
+        let { amt, paid } = req.body;
+        let paidDate = null;
+        // curr Result (SELECT QUERY)
+
+        const currResult = await db.query(
+            `SELECT paid FROM invoices WHERE id = $1`,
+            [id]
         );
-        if (results.rows.length === 0) {
+
+        if (currResult.rows.length === 0) {
             throw new ExpressError(
-                `Can't update invoice with id of ${id}`,
+                `Can't locate invoice with id of ${id}`,
                 404
             );
         }
+
+        const currPaidDate = currResult.rows[0].paid_date;
+
+        if (!currPaidDate && paid) {
+            paidDate = new Date();
+        } else if (!paid) {
+            paidDate = null;
+        } else {
+            paidDate = currPaidDate;
+        }
+
+        const results = await db.query(
+            'UPDATE invoices SET amt=$1, paid=$2, paid_date=$3 WHERE id=$4 RETURNING id, comp_code, amt, paid, add_date, paid_date',
+            [amt, paid, paidDate, id]
+        ); // add paid & paidDate here
+
         return res.send({ invoice: results.rows[0] });
     } catch (e) {
         return next(e);
